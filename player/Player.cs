906c3d85de28453cef6203c21b0e2a17e80eea3a
CobaltHex <cobalthex@live.com>
2025-07-18 00:34:53 -0700

    Doing some wipping

--- player/Player.cs
+++ player/Player.cs
@@ -3,5 +3,13 @@ using System;
 
 public partial class Player : Node3D
 {
-	
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _Process(double delta)
+    {
+        
+    }
 }
